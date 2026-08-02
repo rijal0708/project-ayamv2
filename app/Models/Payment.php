b6f879ec168d7055b7373fa301ec53be8eadd56e
rijal0708 <rijalanshory7@gmail.com>
2026-08-02 23:52:26 +0700

    tambah file payment

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -5,12 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class EggSaleItem extends Model
+class Payment extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'sale_id', 'grade', 'quantity', 'unit_price', 'subtotal',
+        'sale_id', 'date', 'amount', 'method', 'reference_no', 'notes',
     ];
 
     public function sale()
